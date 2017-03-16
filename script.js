a17c752197d6429adce71207bc83d4f9a51c2254
Janelle Zorvan <jzorvan@gmail.com>
2017-03-16 11:59:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+let html = '';
+
+const randomHue = () => {
+  return Math.floor(Math.random() * 256 );
+}
+
+const colorString = () => {
+	let color = 'rgb(';
+	color += randomHue() + ',';
+	color += randomHue() + ',';
+	color += randomHue() + ')';
+	return color;
+}
+
+const print = (message) => {
+	document.write(message);
+}
+
+for (let i = 0; i < 100; i++) {
+  let rgbColor = colorString();
+  html += `<div style="background-color:${rgbColor}">&nbsp</div>`;
+}
+  
+print(html);
